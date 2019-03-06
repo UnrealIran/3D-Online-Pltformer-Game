5086f898a6510496e1e6ceab7d7d21abf5930760
DESKTOP-PDHHLPS\Reza <reza.darooei@gmail.com>
2019-03-06 22:46:20 +0330

    Intial Commit

---
+++ OnlineMasteringGame/Source/OnlineMasteringGame/OnlineMasteringGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OnlineMasteringGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLINEMASTERINGGAME_API AOnlineMasteringGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
