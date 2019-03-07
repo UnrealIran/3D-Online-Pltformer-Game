3a0b83d6b57249857d3e20766dcd81c975393541
DESKTOP-PDHHLPS\Reza <reza.darooei@gmail.com>
2019-03-08 02:00:23 +0330

    Introduction to Property Replication in C+

---
+++ OnlineMasteringGame/Source/OnlineMasteringGame/Private/PlatformerPlayerState.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlatformerPlayerState.h"
+#include "UnrealNetwork.h"
+
+APlatformerPlayerState::APlatformerPlayerState()
+{
+	NumberOfCoinsCollected = 0;
+	MaxHealth = 3;
+
+	CurrentHealth = MaxHealth;
+	NumberOfDeathes = 0;
+
+}
+
+void APlatformerPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(APlatformerPlayerState, NumberOfCoinsCollected);
+	DOREPLIFETIME(APlatformerPlayerState, CurrentHealth);
+	DOREPLIFETIME(APlatformerPlayerState, NumberOfDeathes);
+}
---
+++ OnlineMasteringGame/Source/OnlineMasteringGame/Public/PlatformerPlayerState.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PlatformerPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLINEMASTERINGGAME_API APlatformerPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	APlatformerPlayerState();
+
+protected:
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "Platformer playe State")
+		int NumberOfCoinsCollected;
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "Platformer playe State")
+		int CurrentHealth;
+
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "Platformer playe State")
+		int NumberOfDeathes;
+
+	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadOnly, Category = "Platformer playe State")
+		int MaxHealth;
+
+
+
+
+};
